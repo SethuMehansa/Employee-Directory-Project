15baf1933864002a5bd2ced3c05427bd78578442
SethuMehansa <sethumisethumi@gmail.com>
2025-04-29 10:20:57 +0530

    structure and bean config and employee dto

---
+++ EmployeeDirectory-Backend/src/main/java/com/fortium/edp/dto/Employee.java
@@ -0,0 +1,18 @@
+package com.fortium.edp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    private Long id;
+    private String name;
+    private String email;
+    private String department;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
